e201fe081ea22633c66a89d56d5826a25ced27aa
Hanxi Chen <garychen2000@outlook.com>
2024-01-18 23:41:04 -0500

    First Commit

---
+++ elpa/proof-general-20220329.655/proof-general-pkg.el
@@ -0,0 +1,6 @@
+(define-package "proof-general" "20220329.655" "A generic Emacs interface for proof assistants"
+  '((emacs "25.1"))
+  :commit "2a8701209b273db5a35f15145ec62f32799e03b1" :url "https://proofgeneral.github.io/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
